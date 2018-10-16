3f38f50c6859bdf952a0ebe8ebf16c199b3c2aee
revsic <revsic99@gmail.com>
2018-10-17 02:51:12 +0900

    Update Problem8 - Amstrong Number

--- Math/Problem8/main.cpp
+++ Math/Problem8/main.cpp
@@ -1,6 +1,28 @@
 #include <gsl/gsl>
 
+#include <cmath>
+#include <iostream>
+
+int narcissistic(int number) {
+    int digit = 0;
+    for (int i = 1; number >= i; i *= 10, ++digit);
+
+    int sum = 0;
+    while (number) {
+        sum += pow(number % 10, digit);
+        number /= 10;
+    }
+    return sum;
+}
+
 int main(int argc, char* argv[])
 {
+    for (int i = 100; i < 1000; ++i) {
+        if (i == narcissistic(i)) {
+            std::cout << i << ' ';
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
