993e2810b66a54f37ffe8bc046267820d770cc7f
revsic <revsic99@gmail.com>
2018-10-16 14:55:39 +0900

    Update Math/Problem3 - Least Common Multiplier

--- Math/Problem3/main.cpp
+++ Math/Problem3/main.cpp
@@ -1,6 +1,44 @@
-#include <gsl/gsl>
+// #include <gsl/gsl>
+#include <iostream>
+#include <memory>
+#include <numeric>
 
 int main(int argc, char* argv[])
 {
+    int len = 0;
+    std::cout << "len: ";
+    std::cin >> len;
+
+    if (len < 2) {
+        std::cout << "[*] length should be bigger than two" << std::endl;
+        return 1;
+    }
+
+    int num1 = 0;
+    int num2 = 0;
+    std::cout << "nums: ";
+    std::cin >> num1 >> num2;
+
+    auto log_positive_exception = [] { 
+        std::cout << "[*] number should be positive" << std::endl; 
+    };
+
+    if (num1 <= 0 || num2 <= 0) {
+        log_positive_exception();
+        return 1;
+    }
+
+    int lcm = num1 * num2 / std::gcd(num1, num2);
+    for (int i = 2; i < len; ++i) {
+        std::cin >> num1;
+        if (num1 <= 0) {
+            log_positive_exception();
+            return 1;
+        }
+
+        lcm = lcm * num1 / std::gcd(lcm, num1);
+    }
+    
+    std::cout << "lcm: " << lcm << std::endl;
     return 0;
 }
