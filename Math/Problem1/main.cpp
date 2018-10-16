c9e971f9f1be6597d2b493c1bbf5b597358a4cde
revsic <revsic99@gmail.com>
2018-10-16 14:54:50 +0900

    Update Math/Problem1 - Log either multiplies of 3 or 5

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,6 +1,17 @@
-#include <gsl/gsl>
+// #include <gsl/gsl>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+    int input = 0;
+    std::cin >> input;
+
+    for (int i = 1; i < input; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            std::cout << i << ' ';
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
