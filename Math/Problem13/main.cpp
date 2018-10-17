ab4d5a54cf06c4c4f144e569eef2550a5f583cc2
revsic <revsic99@gmail.com>
2018-10-18 02:13:24 +0900

    Update Problem13 - Calculate PI

--- Math/Problem13/main.cpp
+++ Math/Problem13/main.cpp
@@ -1,6 +1,20 @@
 #include <gsl/gsl>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+    double sum = 1;
+    for (int i = 1; i <= 1000; ++i) {
+        double n = 1 / (2. * i + 1);
+        
+        if (i % 2) {
+            sum -= n;
+        }
+        else {
+            sum += n;
+        }
+    }
+
+    std::cout << sum * 4. << std::endl;
     return 0;
 }
