74748276ade3e1a0bf2157f204bfa0ee7d5134b2
revsic <revsic99@gmail.com>
2018-10-20 10:49:35 +0900

    Update Problem1 - Add sum_of func

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,17 +1,27 @@
 #include <gsl/gsl>
 #include <iostream>
 
+template <int n>
+int sum_of(int target) {
+    while (target % n) --target;
+    target /= n;
+    return n * target * (1 + target) / 2;
+}
+
 int main(int argc, char* argv[])
 {
     int input = 0;
     std::cin >> input;
 
+    int sum1 = sum_of<3>(input - 1) + sum_of<5>(input - 1) - sum_of<15>(input - 1);
+
+    int sum2 = 0;
     for (int i = 1; i < input; ++i) {
         if (i % 3 == 0 || i % 5 == 0) {
-            std::cout << i << ' ';
+            sum2 += i;
         }
     }
-    std::cout << std::endl;
+    std::cout << sum1 << ' ' << sum2 << std::endl;
 
     return 0;
 }
