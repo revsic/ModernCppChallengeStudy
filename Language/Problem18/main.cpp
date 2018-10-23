d209318bbe3976a164de800f542118d809c66d98
revsic <revsic99@gmail.com>
2018-10-23 13:36:27 +0900

    Update Problem18 - Currying fold

--- Language/Problem18/main.cpp
+++ Language/Problem18/main.cpp
@@ -1,27 +1,7 @@
 #include <gsl/gsl>
 #include <iostream>
+#include <tuple>
 #include <utility>
-
-template <typename T>
-auto minimum(T&& arg) {
-    return std::forward<T>(arg);
-}
-
-template <typename T, typename... U>
-auto minimum(T&& given, U&&... args) -> std::common_type_t<T, U...> {
-    auto min = minimum(std::forward<U>(args)...);
-    if (given < min) {
-        return std::forward<T>(given);
-    }
-    else {
-        return min;
-    }
-}
-
-template <typename... T>
-auto minimum(T&&... args) -> std::common_type_t<T...> {
-    return minimum(std::forward<T>(args)...);   
-}
 
 template <typename F>
 struct Fold {
@@ -39,9 +19,9 @@ struct Fold {
             // Do nothing
         }
 
-        template <typename Ts>
-        constexpr auto operator+(Ts&& other) const {
-            return oper(value, other);
+        template <typename Ts, typename W>
+        constexpr friend auto operator+(Ts&& value, const Wrapper<W>& wrapper) {
+            return wrapper.oper(value, wrapper.value);
         }
     };
 
@@ -53,19 +33,18 @@ struct Fold {
     template <typename T, typename... Ts>
     constexpr auto operator()(T&& initial, Ts&&... values) const {
         return impl(
-            std::forward<T>(initial), 
+            std::forward<T>(initial),
             std::make_tuple(Wrapper<Ts>(oper, std::forward<Ts>(values))...),
             std::make_index_sequence<sizeof...(Ts)>{});
     }
 
-    template <typename T, typename Ts, size_t... Idx>
+    template <typename T, typename TupleT, size_t... Idx>
     constexpr auto impl(
         T&& initial, 
-        Ts&& tuple,
+        TupleT&& tuple,
         std::index_sequence<Idx...>) const 
     {
-        constexpr size_t max = sizeof...(Idx) - 1;
-        return (std::get<max - Idx>(tuple) + ... + std::forward<T>(initial));
+        return (std::forward<T>(initial) + ... + std::get<Idx>(tuple));
     }
 };
 
@@ -74,18 +53,20 @@ constexpr auto fold(F&& binary) {
     return Fold<F>(std::forward<F>(binary));
 }
 
+template <typename T, typename U>
+constexpr auto min(const T& a, const U& b) -> std::common_type_t<T, U> {
+    return a < b ? a : b;
+}
+
 constexpr int add(int a, int b) {
     return a + b;
 }
 
 int main() {
-    std::cout << minimum(1, 3, 2, 5, 4) << std::endl;
-    std::cout << minimum(
-                    std::string("abc"), 
-                    std::string("123"), 
-                    std::string("zef")) 
+    std::cout << fold(min<int, int>)(1, 3, 2, 5, 4) << std::endl;
+    std::cout << fold(min<std::string, std::string>)("abc", "zef", "qwe")
               << std::endl;
-    
+
     constexpr int result = fold(add)(1, 2, 3, 4, 5);
     std::cout << result << std::endl;
 
