480c605e496de545ba34f28da412e2943a8b854e
revsic <revsic99@gmail.com>
2018-10-25 12:55:07 +0900

    Update Problem3 - Replace with std::gcd

--- Math/Problem3/main.cpp
+++ Math/Problem3/main.cpp
@@ -1,23 +1,7 @@
 #include <gsl/gsl>
-#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <numeric>
-
-int gcd(int a, int b) {
-    do {
-        if (a > b) {
-            std::swap(a, b);
-        }
-        b %= a;
-    } while (b != 0);
-
-    return a;
-}
-
-int lcm(int a, int b) {
-    return a * b / gcd(a, b);
-}
 
 int main(int argc, char* argv[])
 {
@@ -39,6 +23,7 @@ int main(int argc, char* argv[])
     }
 
     int* data_ptr = data.get();
+    auto lcm = [](int a, int b) { return a * b / std::gcd(a, b); };
     std::cout << std::accumulate(data_ptr, data_ptr + len, 1, lcm) << std::endl;
 
     return 0;
