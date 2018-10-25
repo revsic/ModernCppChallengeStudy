283d6719163a4fba852d075dc17b0c5945161a3c
revsic <revsic99@gmail.com>
2018-10-25 23:45:33 +0900

    Update Problem24 - Add CmakeLists

--- Regex/Problem24/main.cpp
+++ Regex/Problem24/main.cpp
@@ -2,22 +2,19 @@
 #include <iomanip>
 #include <iostream>
 #include <memory>
-#include <sstream>
 
 using BYTE = unsigned char;
 
-auto conversion(const std::string& input) -> std::unique_ptr<BYTE[]> {
-    assert(input.size() % 2);
+auto conversion(const std::string& input) {
+    size_t size = input.size();
+    assert(size % 2 == 0);
 
-    size_t num_bytes = input.size() / 2;
+    size_t num_bytes = size / 2;
     auto ptr = std::make_unique<BYTE[]>(num_bytes);
     
-    std::stringstream sstream(input);
-    for (size_t i = 0; i < num_bytes; ++i) {
-        int byte = 0;
-        sstream >> std::hex >> byte;
-
-        input[i] = static_cast<BYTE>(byte);
+    for (size_t i = 0; i < size; i += 2) {
+        int byte = std::stoi(input.substr(i, 2), nullptr, 16);
+        ptr[i / 2] = static_cast<BYTE>(byte);
     }
 
     return std::make_tuple(std::move(ptr), num_bytes);
@@ -27,10 +24,11 @@ int main(int argc, char* argv[])
 {
     std::string test[] = { "BAADF00D", "baadF00D", "010203040506" };
 
+    std::cout << std::hex << std::setfill('0');
     for (const auto& str : test) {
         auto[ptr, size] = conversion(str);
         for (size_t i = 0; i < size; ++i) {
-            std::cout << std::hex << std::setw(2) << static_cast<int>(ptr[i]) << ' ';
+            std::cout << std::setw(2) << static_cast<int>(ptr[i]) << ' ';
         }
         std::cout << std::endl;
     }
