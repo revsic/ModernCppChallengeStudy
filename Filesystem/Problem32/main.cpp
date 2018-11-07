1cc5c1f38a84842288a03e435908a67d20b64b62
revsic <revsic99@gmail.com>
2018-11-07 11:48:11 +0900

    Update Problem32 - Write pascal's triangle

--- Filesystem/Problem32/main.cpp
+++ Filesystem/Problem32/main.cpp
@@ -1,6 +1,20 @@
 #include <gsl/gsl>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+    int arr[10] = { 1, };
+    for (int i = 1; i <= 10; ++i) {
+        for (int j = i - 1; j > 0; --j) {
+            arr[j] = arr[j] + arr[j - 1];
+        }
+
+        for (int j = 0; j < i; ++j) {
+            std::cout << arr[j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
+    
