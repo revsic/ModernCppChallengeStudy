fc37e3b362c4ad7c96f0528066e6054ee202b4b7
revsic <revsic99@gmail.com>
2018-11-25 00:03:10 +0900

    Update Algorithm - Impl most frequent elem

---
+++ Algorithm/Problem48/main.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+
+template <typename Iter>
+auto most_frequent_elem(Iter begin, Iter end) {
+    using value_type = std::iterator_traits<Iter>::value_type;
+
+    size_t max = 0;
+    std::unordered_map<value_type, size_t> map;
+    for (; begin != end; ++begin) {
+        if (auto find = map.find(*begin); find != map.end()) {
+            auto&[val, num] = *find;
+            num += 1;
+        }
+        else {
+            map[*begin] = 1;
+        }
+        max = std::max(max, map[*begin]);
+    }
+
+    std::vector<std::tuple<value_type, size_t>> vec;
+    for (auto&[val, num] : map) {
+        if (num == max) {
+            vec.emplace_back(val, num);
+        }
+    }
+    return vec;
+}
+
+int main() {
+    int arr[] = { 1, 1, 3, 5, 8, 13, 3, 5, 8, 8, 5 };
+    auto res = most_frequent_elem(std::begin(arr), std::end(arr));
+
+    for (auto const&[val, num] : res) {
+        std::cout << "val: " << val << ", num: " << num << '\n';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
