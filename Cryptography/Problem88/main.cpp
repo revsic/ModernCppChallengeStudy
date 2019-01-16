2e92d68aab1d687e8004afca828341bd4710339a
revsic <revsic99@gmail.com>
2019-01-17 00:40:37 +0900

    Update Cryptography - Add Caesar cipher

--- Cryptography/Problem88/main.cpp
+++ Cryptography/Problem88/main.cpp
@@ -1,6 +1,35 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <string>
+
+bool isupper(char c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+bool islower(char c) {
+    return c >= 'a' && c <= 'z';
+}
+
+std::string encrypt(std::string const& input, size_t dist) {
+    std::string output;
+    output.reserve(input.size());
+
+    for (char c : input) {
+        if (isupper(c)) {
+            c = 'A' + (c - 'A' + dist) % 26;
+        }
+        else if (islower(c)) {
+            c = 'a' + (c - 'a' + dist) % 26;
+        }
+        output.push_back(c);
+    }
+    return output;
+}
 
 int main(int argc, char* argv[])
 {
-    return 0;
+    size_t dist;
+    std::string input;
+    std::cin >> input >> dist;
+
+    std::cout << encrypt(input, dist) << std::endl;
 }
