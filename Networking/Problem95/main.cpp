dbe97c517eb27eafecccab9cff14a61bc7b53e85
revsic <revsic99@gmail.com>
2019-01-19 20:21:36 +0900

    Update Problem95 - Impl dns lookup

--- Networking/Problem95/main.cpp
+++ Networking/Problem95/main.cpp
@@ -1,6 +1,30 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <asio.hpp>
 
 int main(int argc, char* argv[])
 {
+    using resolver = asio::ip::tcp::resolver;
+
+    asio::io_service io_service;
+    resolver resolve(io_service);
+
+    std::string input;
+    std::cin >> input;
+
+    resolve.async_resolve(input, "https", 
+        [](asio::error_code const& err, resolver::iterator iter) {
+            if (err) {
+                std::cout << "Error occured: " << err << std::endl;
+                return;
+            }
+
+            std::cout << "Result:" << std::endl;
+            for(; iter != resolver::iterator(); ++iter) {
+                asio::ip::tcp::endpoint endpoint = *iter;
+                std::cout << endpoint << std::endl;
+            }
+        });
+
+    io_service.run();
     return 0;
 }
